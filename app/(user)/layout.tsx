7f934079a33e5f9295a7424463d9f8db6e121c3f
vikaswakde <vikaswakdepc@gmail.com>
2024-12-12 07:46:00 +0530

    solved enoent error by removing root page.tsx

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,7 +1,13 @@
-import { Navbar } from "@/app/components/Navbar";
 import { WhatsAppButton } from "@/app/components/WhatsAppButton";
-import Head from "next/head";
-import Footer from "../components/Footer";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Your Page Title",
+  description: "Description of your page",
+  icons: {
+    icon: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css",
+  },
+};
 
 export default function UserLayout({
   children,
@@ -10,18 +16,8 @@ export default function UserLayout({
 }) {
   return (
     <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-          integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMt23jFh2bP3D7mV7W7T8cM7yU1lVZ5L5r5G3"
-          crossOrigin="anonymous"
-        />
-      </Head>
-      <Navbar />
-      <main className="">{children}</main>
+      <main>{children}</main>
       <WhatsAppButton />
-      <Footer />
     </>
   );
 }
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-// import Image from "next/image";
-
-import Home from "./(user)/page";
-
-export default function RootPage() {
-  return (
-    <Home />
-  );
-}
